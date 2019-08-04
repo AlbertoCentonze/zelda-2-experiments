deaeb3934db3da755adae1eea5fd0db122cf3e40
Alberto <albicento.ac@gmail.com>
2019-08-05 01:35:36 +0200

    05.08.19 - First Maps and Movement Scripts
    Imported every spritesheet I need for testing.
    Added 2 test scene, one for the 2d (with a good character controller and movement script) view and one for the overworld. Got working a few animations and starting to build a dialogue system

---
+++ Assets/Scripts/OverworldMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldMovement : MonoBehaviour
+{
+    private Transform player;
+    [SerializeField] private float speed = 5f; 
+
+    void Awake()
+    {
+        player = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == - 1)
+        {
+            player.position = new Vector3(speed * Time.deltaTime * Input.GetAxisRaw("Horizontal"), player.position.y, player.position.z);
+        }
+        else if (Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            player.position = new Vector3(player.position.x, speed * Time.deltaTime * Input.GetAxisRaw("Vertical"), player.position.z);
+        }
+    }
+}
